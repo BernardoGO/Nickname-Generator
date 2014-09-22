7b093740f447eea940b379f6c0d8f5ed5d207599
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2014-09-22 20:00:45 -0300

    reading lines

--- run.py
+++ run.py
@@ -4,4 +4,20 @@ __author__ = 'bernardo'
 import linecache
 import random
 
-print linecache.getline('wordlist.txt', 4)
+wordsfile = 'wordlist.txt'
+
+getline = lambda x: linecache.getline(wordsfile, x).replace("\n", "")
+
+infile=open(wordsfile, "r")
+count=0
+for line in infile:
+    count=count+1
+
+xs = random.randint(0, count)
+print xs
+word1 = getline(0)
+word2 = getline(random.randint(0, count))
+
+
+print word1+word2
+
